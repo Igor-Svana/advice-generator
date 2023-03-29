38d750fec23454cfa6a7aebf9ff77b432d3f12d9
Igor-Svana <igor.svana@gmail.com>
2023-03-29 21:41:48 +0200

    fix

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export const fetchAdvice = async() => {
     const response = await axios.get(import.meta.env.VITE_API_URL)
-    console.log(response.data.slip)
     return response.data;
 }
